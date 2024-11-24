098990317a9d5532eb4a943d485602e082dfa649
Vitalii Artiukh <radial_car@yahoo.com>
2024-11-24 21:06:42 +0200

    fixed

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,7 @@
-function merge(objA: object, objB: object) {
+function merge<T extends object, P extends object>(objA: T, objB: P) {
   return Object.assign(objA, objB);
 }
+
+const merged = merge({ name: "Alisa" }, { age: 28 });
+
+console.log(merged);
